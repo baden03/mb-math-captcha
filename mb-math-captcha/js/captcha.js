a4d9025ba0acc172cdac01d3c0cfac89914392b1
Baden <baden03@gmail.com>
2025-06-06 13:05:37 +0200

    Added rate limiting and updating of math challenge on wrong answers
    Rate limiting
    Single-digit math challenges
    Singleton pattern implementation
    Error handling
    Transient cleanup
    Code organization improvements

--- mb-math-captcha/js/captcha.js
+++ mb-math-captcha/js/captcha.js
@@ -3,6 +3,7 @@ jQuery(document).ready(function($) {
     $.validator.addMethod('math_captcha', function(value, element) {
         var $element = $(element);
         var token = $element.data('token');
+        var $challenge = $('#' + $element.attr('id') + '-challenge');
         
         // Make the AJAX call
         var result = false;
@@ -19,9 +20,36 @@ jQuery(document).ready(function($) {
             },
             async: false,
             success: function(response) {
-                result = response === 'true';
-                if (!result && response.data) {
+                if (response.success === false) {
+                    result = false;
                     errorMessage = response.data;
+                    
+                    // Get a new challenge on failed attempt
+                    $.ajax({
+                        url: mbMathCaptcha.ajaxurl,
+                        type: 'POST',
+                        data: {
+                            action: 'get_new_challenge',
+                            nonce: mbMathCaptcha.nonce
+                        },
+                        async: false,
+                        success: function(newChallenge) {
+                            if (newChallenge.success) {
+                                // Update the token
+                                $element.data('token', newChallenge.data.token);
+                                
+                                // Update the challenge text
+                                $challenge.text(
+                                    mbMathCaptcha.i18n.solve_math_problem.replace('%s', newChallenge.data.question)
+                                );
+                                
+                                // Clear the input
+                                $element.val('');
+                            }
+                        }
+                    });
+                } else {
+                    result = response === 'true';
                 }
             }
         });
